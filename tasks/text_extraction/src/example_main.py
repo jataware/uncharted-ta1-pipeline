f123fd6da21fee6dc0282f87c83d2af2df57874c
Chris Bethune <cbethune@uncharted.software>
2023-11-11 12:36:08 -0500

    fixes typing issues
    fixes typing issues and updates data structures

--- tasks/text_extraction/src/example_main.py
+++ tasks/text_extraction/src/example_main.py
@@ -1,28 +1,27 @@
-
 import os
 import logging, json
 
-from ocr.google_vision_ocr import GoogleVisionOCR
-from text_extractor import ResizeTextExtractor, TileTextExtractor
-from image_io import load_pil_image, normalize_image_format
+from .ocr.google_vision_ocr import GoogleVisionOCR
+from .text_extractor import ResizeTextExtractor, TileTextExtractor
+from .image_io import load_pil_image, normalize_image_format
 
 # ENV VARIABLE -- needed for google-vision API
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/path/to/google/vision/creds/json/file'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/path/to/google/vision/creds/json/file"
+
 
 def run():
-
     # Example code to load an image and perform text extraction
-    image_path = 'path/to/input/image'
-    logging.info('')
-    logging.info(f'Running text extraction on image {image_path}')
-    
+    image_path = "path/to/input/image"
+    logging.info("")
+    logging.info(f"Running text extraction on image {image_path}")
+
     im = load_pil_image(image_path)
     im = normalize_image_format(im)
 
     # text-extraction with image scaling...
-    text_extractor = ResizeTextExtractor() 
+    text_extractor = ResizeTextExtractor()
     # OR, text-extraction with image tiling...
-    #text_extractor = TileTextExtractor()
+    # text_extractor = TileTextExtractor()
 
     ocr_results = text_extractor.run(im)
 
@@ -30,11 +29,14 @@ def run():
     results_json = json.dumps([res.model_dump() for res in ocr_results])
     print(results_json)
 
-    logging.info('Done!')
+    logging.info("Done!")
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format=f'%(asctime)s %(levelname)s %(name)s\t: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format=f"%(asctime)s %(levelname)s %(name)s\t: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
     run()
-
--- tasks/text_extraction/src/text_extractor.py
+++ tasks/text_extraction/src/text_extractor.py
@@ -1,67 +1,72 @@
-
 import numpy as np
 import os, logging
 from math import ceil
+from typing import Tuple, List, Dict, Any
+from pathlib import Path
 from PIL import Image
-
-from ocr.google_vision_ocr import GoogleVisionOCR
-from schema.page_extraction import PageExtraction
-from schema.extraction_identifier import ExtractionIdentifier
-from tile import Tile
-
+from PIL.Image import Image as PILImage
+from .ocr.google_vision_ocr import GoogleVisionOCR
+from .entities import DocTextExtraction, TextExtraction, Point, Tile
 
 # ENV VARIABLE -- needed for google-vision API
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/path/to/google/vision/creds/json/file'
 
-PIXEL_LIM_DEFAULT = 6000    # default max pixel limit for input image (determines amount of image resizing)
+PIXEL_LIM_DEFAULT = 6000  # default max pixel limit for input image (determines amount of image resizing)
 
 logger = logging.getLogger(__name__)
 
 
-class TextExtractor():
-    '''
+class TextExtractor:
+    """
     Base class for OCR-based text extraction
-    '''
+    """
 
-    def __init__(self):
-        self.ocr = GoogleVisionOCR()
-        self.model_id = 'google-cloud-vision'
-    
+    def __init__(
+        self, cache_dir: Path, to_blocks: bool = True, document_ocr: bool = False
+    ):
+        self._ocr = GoogleVisionOCR()
+        self._model_id = "google-cloud-vision"
+        self._to_blocks = to_blocks
+        self._document_ocr = document_ocr
+        self._cache_dir = cache_dir
 
-    def _extract_text(self, im: Image.Image, to_blocks: bool=True, document_ocr: bool=False) -> list[dict[str, any]]:
-
+    def _extract_text(self, im: PILImage) -> List[Dict[str, Any]]:
         img_gv = GoogleVisionOCR.pil_to_vision_image(im)
 
-        #----- do GoogleVision OCR
+        # ----- do GoogleVision OCR
         ocr_texts = []
-        if document_ocr:
-            ocr_texts = self.ocr.detect_document_text(img_gv)
+        if self._document_ocr:
+            ocr_texts = self._ocr.detect_document_text(img_gv)
         else:
-            ocr_texts = self.ocr.detect_text(img_gv)
-            if to_blocks:
-                ocr_texts = self.ocr.text_to_blocks(ocr_texts)
+            ocr_texts = self._ocr.detect_text(img_gv)
+            if self._to_blocks:
+                ocr_texts = self._ocr.text_to_blocks(ocr_texts)
 
         return ocr_texts
-    
 
-    def run(self):
+    def process(self):
         # override in inherited classes below
         raise NotImplementedError
-    
+
 
 class ResizeTextExtractor(TextExtractor):
-    '''
+    """
     OCR-based text extraction with optional image scaling prior to OCR
-    '''
+    """
 
-    def __init__(self, pixel_lim: int=PIXEL_LIM_DEFAULT):
-        super().__init__()
-        self.pixel_lim = pixel_lim
-        self.model_id += f'-resize{pixel_lim}'
+    def __init__(
+        self,
+        cache_dir: Path,
+        to_blocks: bool,
+        document_ocr: bool,
+        pixel_lim: int = PIXEL_LIM_DEFAULT,
+    ):
+        super().__init__(cache_dir, to_blocks, document_ocr)
+        self._pixel_lim = pixel_lim
+        self._model_id += f"resize-{pixel_lim}"
 
-
-    def run(self, im: Image.Image, to_blocks: bool=True, document_ocr: bool=False) -> list[PageExtraction]:
-        '''
+    def process(self, doc_id: str, im: PILImage) -> DocTextExtraction:
+        """
         Run OCR-based text extractor
         Image may be internally scaled prior to OCR, if needed
 
@@ -70,65 +75,66 @@ class ResizeTextExtractor(TextExtractor):
             to_blocks: =True; group OCR results into blocks/lines of text related text
             document_ocr: =False; use 'document level' OCR, meant for images with dense paragraphs/columns of text
         Returns:
-            List of PageExtraction objects
+            DocumentTextExtraction object
             (in pixel coords of full-sized image, not resized pixel coords)
-        ''' 
-        #im_orig_size = im.size   #(width, height)
+        """
+        # im_orig_size = im.size   #(width, height)
         im_resized, im_resize_ratio = self._resize_image(im)
 
-        ocr_blocks = self._extract_text(im_resized, to_blocks, document_ocr)
+        ocr_blocks = self._extract_text(im_resized)
 
         # scale OCR pixel co-ords back to original image dimensions
         if ocr_blocks and im_resize_ratio < 1.0 and im_resize_ratio > 0.0:
-            ocr_blocks = GoogleVisionOCR.scale_ocr_coords(ocr_blocks, 1.0/im_resize_ratio)
+            ocr_blocks = GoogleVisionOCR.scale_ocr_coords(
+                ocr_blocks, 1.0 / im_resize_ratio
+            )
 
-        # convert OCR results to TA1 schema
-        # TODO -- could add in confidence per ocr block? (google vision seems to return score=0.0?)
-        model_field_name = 'document_ocr' if document_ocr else 'ocr'
-
-        results = []
+        # convert output to internal schema
+        texts: List[TextExtraction] = []
         for ocr_block in ocr_blocks:
-            ocr_result = PageExtraction(
-                name='ocr',
-                bounds=GoogleVisionOCR.bounding_polygon_to_list(ocr_block['bounding_poly']),
-                ocr_text = ocr_block['text'],
-                model=ExtractionIdentifier(model=self.model_id, field=model_field_name))
-            results.append(ocr_result)
+            bounds = [
+                Point(x=vertex.x, y=vertex.y)
+                for vertex in ocr_block["bounding_poly"].vertices
+            ]
+            ocr_result = TextExtraction(
+                text=ocr_block["text"], confidence=1.0, bounds=bounds
+            )
+            texts.append(ocr_result)
 
-        return results
-    
+        return DocTextExtraction(doc_id=f"{doc_id}-{self._model_id}", extractions=texts)
 
-    def _resize_image(self, im: Image.Image) -> tuple[Image.Image, float]:
-        '''
-        Resize an image, if needed, so max dimension is <= self.pixel_lim
-        '''
+    def _resize_image(self, im: PILImage) -> Tuple[PILImage, float]:
+        """
+        Resize an image, if needed, so max dimension is <= self._pixel_lim
+        """
         # TODO could be moved to a 'common' module?
 
-        im_orig_size = im.size   #(width, height)
+        im_orig_size = im.size  # (width, height)
         im_resize_ratio = 1.0
-        if max(im_orig_size) > self.pixel_lim:
-            im_resize_ratio = self.pixel_lim / max(im_orig_size)
-            logger.info('Resizing image with ratio: {}'.format(im_resize_ratio))
+        if max(im_orig_size) > self._pixel_lim:
+            im_resize_ratio = self._pixel_lim / max(im_orig_size)
+            logger.info("Resizing image with ratio: {}".format(im_resize_ratio))
 
-            reduced_size = int(im_orig_size[0] * im_resize_ratio), int(im_orig_size[1] * im_resize_ratio)   
+            reduced_size = int(im_orig_size[0] * im_resize_ratio), int(
+                im_orig_size[1] * im_resize_ratio
+            )
             im = im.resize(reduced_size, Image.Resampling.LANCZOS)
-        
-        return im, im_resize_ratio    
+
+        return im, im_resize_ratio
 
 
 class TileTextExtractor(TextExtractor):
-    '''
+    """
     OCR-based text extraction with image tiling prior to OCR
-    '''
-    
-    def __init__(self, split_lim: int=PIXEL_LIM_DEFAULT):
-        super().__init__()
-        self.split_lim = split_lim
-        self.model_id += f'-tile{split_lim}'
-    
+    """
 
-    def run(self, im: Image.Image, to_blocks: bool=True, document_ocr: bool=False) -> list[PageExtraction]:
-        '''
+    def __init__(self, cache_dir: Path, split_lim: int = PIXEL_LIM_DEFAULT):
+        super().__init__(cache_dir)
+        self.split_lim = split_lim
+        self._model_id += f"tile-{split_lim}"
+
+    def process(self, im: PILImage, doc_id: str) -> DocTextExtraction:
+        """
         Run OCR-based text extractor
         Image may be internally tiled prior to OCR, if needed
 
@@ -139,67 +145,70 @@ class TileTextExtractor(TextExtractor):
         Returns:
             List of PageExtraction objects
             (in pixel coords of full-sized image, not resized pixel coords)
-        ''' 
+        """
 
         # TODO -- this code could be modified to include overlap/stride len, etc.
         # (then, any overlapping OCR results need to be de-dup'd)
 
         im_tiles = self._split_image(im, self.split_lim)
-        logger.info(f'Image split into {len(im_tiles)} tiles. Extracting OCR text from each...')
+        logger.info(
+            f"Image split into {len(im_tiles)} tiles. Extracting OCR text from each..."
+        )
 
-        ocr_blocks = [] # list for OCR results across all tiles (whole image)
+        ocr_blocks: List[
+            Dict[str, Any]
+        ] = []  # list for OCR results across all tiles (whole image)
         for tile in im_tiles:
             # get OCR results for this tile
-            tile_ocr_blocks = self._extract_text(tile.image, to_blocks, document_ocr)
+            tile_ocr_blocks = self._extract_text(tile.image)
             # convert OCR poly-bounds to global pixel coords and add to results
-            ocr_blocks.extend(GoogleVisionOCR.offset_ocr_coords(tile_ocr_blocks, tile.coordinates))
+            ocr_blocks.extend(
+                GoogleVisionOCR.offset_ocr_coords(tile_ocr_blocks, tile.coordinates)
+            )
 
         # convert OCR results to TA1 schema
-        # TODO -- could add in confidence per ocr block? (google vision seems to return score=0.0?)
-        model_field_name = 'document_ocr' if document_ocr else 'ocr'
-
-        results = []
+        texts: List[TextExtraction] = []
         for ocr_block in ocr_blocks:
-            ocr_result = PageExtraction(
-                name='ocr',
-                bounds=GoogleVisionOCR.bounding_polygon_to_list(ocr_block['bounding_poly']),
-                ocr_text = ocr_block['text'],
-                model=ExtractionIdentifier(model=self.model_id, field=model_field_name))
-            results.append(ocr_result)
+            bounds = [
+                Point(x=vertex.x, y=vertex.y)
+                for vertex in ocr_block["bounding_poly"].vertices
+            ]
+            ocr_result = TextExtraction(
+                text=ocr_block["text"], confidence=1.0, bounds=bounds
+            )
+            texts.append(ocr_result)
+        return DocTextExtraction(doc_id=f"{doc_id}-{self._model_id}", extractions=texts)
 
-        return results
-
-
-    def _split_image(self, image: Image.Image, size_limit: int) -> list[Tile]:
-        '''
+    def _split_image(self, image: PILImage, size_limit: int) -> List[Tile]:
+        """
         split an image as needed to fit under the image size limit for x and y
-        '''
+        """
 
         image_size = image.size
         splits_x = self._get_splits(image_size[0], size_limit)
         splits_y = self._get_splits(image_size[1], size_limit)
-        images = []
+        images: List[Tile] = []
         for split_y in splits_y:
             for split_x in splits_x:
-                ims = Image.new(mode="RGB", size=(split_x[1] - split_x[0], split_y[1] - split_y[0]))
+                ims = Image.new(
+                    mode="RGB", size=(split_x[1] - split_x[0], split_y[1] - split_y[0])
+                )
                 cropping = image.crop((split_x[0], split_y[0], split_x[1], split_y[1]))
                 ims.paste(cropping, (0, 0))
                 images.append(Tile(ims, (split_x[0], split_y[0])))
         return images
 
-
-    def _get_splits(self, size: int, limit: int) -> list[tuple]:
-        '''
-        get the pixel intervals for image tiling 
+    def _get_splits(self, size: int, limit: int) -> List[Tuple]:
+        """
+        get the pixel intervals for image tiling
         note, currently the tile stride == limit (0% overlap)
-        '''
+        """
         splits = ceil(float(size) / limit)
         split_inc = ceil(float(size) / splits)
-        split_vals = []
+        split_vals: List[Tuple[int, float]] = []
         current = 0
         while current < size:
             next_inc = min(current + split_inc, size)
             split_vals.append((current, next_inc))
             current = next_inc
         return split_vals
-    
