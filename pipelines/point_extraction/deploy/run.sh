dac041e64169809953969498aaca88d61d9f50b9
Chris Bethune <cbethune@uncharted.software>
2023-12-11 00:33:54 -0500

    adds docker container generation scripts

---
+++ pipelines/point_extraction/deploy/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# args: $1 - path to local directory to mount as /workdir in docker container
+# args: $2 - s3 url pointing to model folder
+
+docker network create lara
+docker run \
+    --runtime=nvidia \
+    --gpus all \
+    --rm \
+    --name point_extraction \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -v $1:/workdir \
+    --net lara \
+    -p 5000:5000 \
+    docker.uncharted.software/point_extraction:latest --workdir /workdir --model $2
