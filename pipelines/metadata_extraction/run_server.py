ddc1f6b4d6623eecb83cf34ef3d66b1f8c628aa0
Chris Bethune <cbethune@uncharted.software>
2023-12-12 01:09:24 -0500

    fixes vision OCR env var setup for docker container

--- pipelines/metadata_extraction/run_server.py
+++ pipelines/metadata_extraction/run_server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, Response
 import logging, json
 from hashlib import sha1
@@ -8,6 +9,7 @@ from pipelines.metadata_extraction.metadata_extraction_pipeline import (
     MetadataExtractorPipeline,
 )
 from tasks.common.pipeline import PipelineInput, BaseModelOutput
+from tasks.metadata_extraction.entities import METADATA_EXTRACTION_OUTPUT_KEY
 
 app = Flask(__name__)
 
@@ -35,8 +37,7 @@ def process_image():
             msg = "No metadata extracted"
             logging.warning(msg)
             return (msg, 500)
-
-        metadata_result = result["metadata_extraction_output"]
+        metadata_result = result[METADATA_EXTRACTION_OUTPUT_KEY]
         if isinstance(metadata_result, BaseModelOutput):
             # convert result to a JSON array
             result_json = json.dumps(metadata_result.data.model_dump())
@@ -67,7 +68,7 @@ if __name__ == "__main__":
         format=f"%(asctime)s %(levelname)s %(name)s\t: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    logger = logging.getLogger("segmenter app")
+    logger = logging.getLogger("metadata extraction app")
     logger.info("*** Starting map metadata app ***")
 
     # init segmenter
