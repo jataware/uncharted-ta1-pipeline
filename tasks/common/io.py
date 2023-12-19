1829d697568319cef3274f1a2051cd72d72bd65f
Chris Bethune <cbethune@uncharted.software>
2023-12-19 12:25:01 -0500

    adds s3 support to the input iterator

--- tasks/common/io.py
+++ tasks/common/io.py
@@ -1,35 +1,45 @@
-from pathlib import Path
+import io
 import os
 import re
 import json
+from urllib.parse import urlparse
+from enum import Enum
+from pathlib import Path
 from pydantic import BaseModel
 from typing import Iterator, List, Tuple, Sequence, Union
 from PIL.Image import Image as PILImage
 from PIL import Image
 import boto3
 
-
 # https://stackoverflow.com/questions/51152059/pillow-in-python-wont-let-me-open-image-exceeds-limit
 Image.MAX_IMAGE_PIXELS = 400000000  # to allow PIL to load large images
-
 
 # regex for matching s3 uris
 S3_URI_MATCHER = re.compile(r"^s3://[a-zA-Z0-9.-]+$")
 
 
+class Mode(Enum):
+    FILE = 1
+    S3_URI = 2
+    URL = 3
+
+
 class ImageFileInputIterator(Iterator[Tuple[str, PILImage]]):
     """Generates an iterable list of PIL images from a directory of images"""
 
-    def __init__(self, image_path: Path) -> None:
+    def __init__(self, image_path: str) -> None:
         """Initializes the iterator"""
-        self._image_files: List[Path] = []
+
+        self._image_files: List[str] = []
         self._index = 0
 
-        # recursivley traverse the input directory and find all image files
-        for root, _, files in os.walk(image_path):
-            for file in files:
-                self._image_files.append(Path(os.path.join(root, file)))
-        self._image_files.sort()
+        # check if the string is an s3 uri or a file path and collect up the
+        # locations of the files to be loaded
+        mode = self._get_file_source(image_path)
+        if mode == Mode.S3_URI or mode == Mode.URL:
+            self._s3_init(image_path)
+        else:
+            self._file_init(image_path)
 
     def __iter__(self):
         return self
@@ -39,15 +49,84 @@ class ImageFileInputIterator(Iterator[Tuple[str, PILImage]]):
         if self._index < len(self._image_files):
             image_path = self._image_files[self._index]
             self._index += 1
-            if self._verify_is_image(image_path):
-                image = Image.open(image_path)
-                doc_id = image_path.stem
+
+            mode = self._get_file_source(image_path)
+            if mode == Mode.S3_URI or mode == Mode.URL:
+                # process the image from s3
+                image = self._load_s3(image_path)
+                doc_id = image_path.split("/")[-1].split(".")[0]
+                return (doc_id, image)
+            elif self._verify_is_image(Path(image_path)):
+                # process the image from the local file system
+                image = self._load_file(image_path)
+                doc_id = image_path.split("/")[-1].split(".")[0]
                 return (doc_id, image)
             return self.__next__()
         else:
             raise StopIteration
 
+    def _load_file(self, path: str) -> PILImage:
+        """Loads an image file into memory as a PIL image object"""
+        return Image.open(path)
+
+    def _load_s3(self, path: str) -> PILImage:
+        """Loads an image file from s3 into memory as a PIL image object"""
+        # extract bucket and prefix string from path
+        bucket = path.split("/")[3]
+        key = "/".join(path.split("/")[4:])
+        # load image from s3
+        s3 = boto3.resource("s3")
+        obj = s3.Object(bucket, key)
+        img_bytes_io = io.BytesIO(obj.get()["Body"].read())
+        image = Image.open(img_bytes_io)
+        return image
+
+    def _get_file_source(self, path: str) -> Mode:
+        """Checks if the path is a file, s3 uri, or url"""
+        if S3_URI_MATCHER.match(path):
+            return Mode.S3_URI
+        elif urlparse(path).scheme == "http" or urlparse(path).scheme == "https":
+            return Mode.URL
+        else:
+            return Mode.FILE
+
+    def _file_init(self, path: str):
+        """Initializes the iterator with a list of local image files"""
+
+        # recursivley traverse the input directory and find all image files
+        for root, _, files in os.walk(path):
+            for file in files:
+                self._image_files.append(os.path.join(root, file))
+        self._image_files.sort()
+
+    def _s3_init(self, path: str):
+        """Initializes the iterator with a list of s3 image files"""
+
+        # create s3 client
+        client = boto3.client("s3")  # type: ignore
+        # extract bucket and prefix string from path
+        split_path = path.split("/")
+        if self._get_file_source(path) == Mode.URL:
+            parsed_url = urlparse(path)
+            source = parsed_url.scheme + "://" + parsed_url.netloc
+            bucket = split_path[3]
+            prefix = "/".join(split_path[4:])
+        else:
+            source = "s3"
+            bucket = split_path[2]
+            prefix = "/".join(split_path[3:])
+
+        # list objects in bucket with prefix
+        objects = client.list_objects(Bucket=bucket, Prefix=prefix)
+        # add all objects to the list of image files
+        for obj in objects["Contents"]:
+            if "Key" in obj:
+                key = obj["Key"]
+                self._image_files.append(f"{source}/{bucket}/{key}")
+        self._image_files.sort()
+
     def _verify_is_image(self, image_path: Path) -> bool:
+        """Verifies that the file at the given path is an image"""
         try:
             im = Image.open(image_path)
             im.verify()
--- tasks/common/test/test_io.py
+++ tasks/common/test/test_io.py
@@ -1,10 +1,14 @@
 import os
+import os.path as path
 import json
+from io import BytesIO
 from pathlib import Path
 from PIL import Image
-from tasks.common.io import ImageFileInputIterator, JSONFileWriter
 from pydantic import BaseModel
 from typing import Dict, List
+import boto3
+from moto import mock_s3
+from tasks.common.io import ImageFileInputIterator, JSONFileWriter
 
 
 class TestData(BaseModel):
@@ -12,7 +16,7 @@ class TestData(BaseModel):
     color: str
 
 
-def test_image_file_input_iterator():
+def test_image_file_input_iterator_filesystem():
     # Create a temporary directory and save some test images
     test_dir = Path("tasks/common/test/data")
     test_dir.mkdir(parents=True, exist_ok=True)
@@ -46,7 +50,66 @@ def test_image_file_input_iterator():
     test_dir.rmdir()
 
 
-def test_json_file_writer():
+# add a test for s3 input iterator
+@mock_s3
+def image_file_input_iterator_s3(iterator: ImageFileInputIterator):
+    # Create a temporary directory and save some test images
+    test_prefix = "data"
+    test_bucket = "test-bucket"
+
+    conn = boto3.resource("s3", region_name="us-east-1")
+    bucket = conn.create_bucket(Bucket=test_bucket)
+
+    image1 = Image.new("RGB", (100, 100), color="red")
+    image1_io = BytesIO()
+    image1.save(image1_io, format="png")
+    image1_io.seek(0)
+
+    image2 = Image.new("RGB", (100, 100), color="blue")
+    image2_io = BytesIO()
+    image2.save(image2_io, format="png")
+    image2_io.seek(0)
+
+    bucket.put_object(
+        Key=path.join(test_prefix, "image1.png"),
+        Body=image1_io,
+    )
+    bucket.put_object(
+        Key=path.join(test_prefix, "image2.png"),
+        Body=image2_io,
+    )
+
+    # Iterate over the images and verify the results
+    expected_images = [
+        ("image1", image1),
+        ("image2", image2),
+    ]
+    for expected_doc_id, expected_image in expected_images:
+        doc_id, image = next(iterator)
+        assert doc_id == expected_doc_id
+        assert list(image.getdata()) == list(expected_image.getdata())
+
+    # Verify that StopIteration is raised when there are no more images
+    try:
+        next(iterator)
+        assert False, "StopIteration should have been raised"
+    except StopIteration:
+        pass
+
+
+def test_json_file_writer_s3_uri():
+    # Initialize the ImageFileInputIterator with the test directory
+    image_file_input_iterator_s3(ImageFileInputIterator(str("s3://test-bucket/data")))
+
+
+def test_json_file_writer_s3_url():
+    # Initialize the ImageFileInputIterator with the test directory
+    image_file_input_iterator_s3(
+        ImageFileInputIterator(str("https://buckets/test-bucket/data"))
+    )
+
+
+def test_json_file_writer_filesystem():
     # Test writing a single BaseModel instance
     test_data = TestData(name="test", color="red")
     output_location = "tasks/common/test/data/test.json"
