d6cd56e7609c213f385c473a6981744918ce709d
Chris Bethune <cbethune@uncharted.software>
2024-05-01 16:49:54 -0400

    adds workdir as a parameter

--- pipelines/point_extraction/deploy/run_eval.sh
+++ pipelines/point_extraction/deploy/run_eval.sh
@@ -3,6 +3,7 @@
 # args: $1 - path to local directory to mount as /input in docker container
 #       $2 - path to local directory to mount as /output in docker container
 #       $3 - path to local directory to mount as /hints in docker container
+#       $4 - path to local directory to mount as /workdir in docker container
 
 # ensure that an the GOOGLE_APPLICATION_CREDENTIALS env var is set
 if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
@@ -11,8 +12,8 @@ if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
 fi
 
 # ensure that the args are present
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "Usage: $0 <input_dir> <output_dir> <hints_dir>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+    echo "Usage: $0 <input_dir> <output_dir> <hints_dir> <workdir_dir>"
     exit 1
 fi
 
@@ -31,6 +32,7 @@ docker run \
          -m pipelines.point_extraction.run_pipeline \
         --input /input \
         --output /output \
+        --workdir /workdir \
         --legend_hints /hints \
         --bitmasks \
         --model_point_extractor pipelines/point_extraction_weights/lara_yolo_20240425_dryrun_best.pt \
