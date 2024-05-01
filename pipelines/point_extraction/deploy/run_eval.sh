c9bdb88df0f4521ba05735abb75b538741980a66
Chris Bethune <cbethune@uncharted.software>
2024-05-01 15:03:58 -0400

    adds script to run in aicma contest model to support may 2024 dry run eval

---
+++ pipelines/point_extraction/deploy/run_eval.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# args: $1 - path to local directory to mount as /input in docker container
+#       $2 - path to local directory to mount as /output in docker container
+#       $3 - path to local directory to mount as /hints in docker container
+
+# ensure that an the GOOGLE_APPLICATION_CREDENTIALS env var is set
+if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+    echo "GOOGLE_APPLICATION_CREDENTIALS env var must be set to the path of the GCP service account key"
+    exit 1
+fi
+
+# ensure that the args are present
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Usage: $0 <input_dir> <output_dir> <hints_dir>"
+    exit 1
+fi
+
+docker network ls | grep -q 'lara' || docker network create lara
+docker run \
+    --rm \
+    --name point_extraction \
+    --entrypoint python3 \
+    -e GOOGLE_APPLICATION_CREDENTIALS=/credentials.json \
+    -v $GOOGLE_APPLICATION_CREDENTIALS:/credentials.json \
+    -v $1:/input \
+    -v $2:/output \
+    -v $3:/hints \
+    --net lara \
+    uncharted/lara-point-extract:dry-run \
+         -m pipelines.point_extraction.run_pipeline \
+        --input /input \
+        --output /output \
+        --legend_hints /hints \
+        --bitmasks \
+        --model_point_extractor pipelines/point_extraction_weights/lara_yolo_20240425_dryrun_best.pt \
+        --model_segmenter pipelines/segmentation_weights/layoutlmv3_xsection_20231201
