bd895e3230a3e00dc04f7af59a49a674748c7cce
Chris Bethune <cbethune@uncharted.software>
2024-07-15 21:30:22 -0400

    updates models in eval run run script and makes legend the last arg

--- pipelines/point_extraction/deploy/run_eval.sh
+++ pipelines/point_extraction/deploy/run_eval.sh
@@ -2,8 +2,8 @@
 
 # args: $1 - path to local directory to mount as /input in docker container
 #       $2 - path to local directory to mount as /output in docker container
-#       $3 - path to local directory to mount as /hints in docker container
-#       $4 - path to local directory to mount as /workdir in docker container
+#       $3 - path to local directory to mount as /workdir in docker container
+#       $4 - path to local directory to mount as /hints in docker container
 
 # ensure that an the GOOGLE_APPLICATION_CREDENTIALS env var is set
 if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
@@ -13,7 +13,7 @@ fi
 
 # ensure that the args are present
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
-    echo "Usage: $0 <input_dir> <output_dir> <hints_dir> <workdir_dir>"
+    echo "Usage: $0 <input_dir> <output_dir> <workdir_dir> <hints_dir>"
     exit 1
 fi
 
@@ -27,14 +27,15 @@ docker run \
     -v $GOOGLE_APPLICATION_CREDENTIALS:/credentials.json \
     -v $1:/input \
     -v $2:/output \
-    -v $3:/hints \
+    -v $3:/workdir \
+    -v $4:/hints \
     --net lara \
-    uncharted/lara-point-extract:dry-run \
+    uncharted/lara-point-extract:eval \
          -m pipelines.point_extraction.run_pipeline \
         --input /input \
         --output /output \
         --workdir /workdir \
         --legend_hints /hints \
         --bitmasks \
-        --model_point_extractor pipelines/point_extraction_weights/lara_yolo_20240425_dryrun_best.pt \
-        --model_segmenter pipelines/segmentation_weights/layoutlmv3_xsection_20231201
+        --model_point_extractor pipelines/point_extraction_weights/lara_yolo9_20240702_best.pt \
+        --model_segmenter pipelines/segmentation_weights/layoutlmv3_20240531
