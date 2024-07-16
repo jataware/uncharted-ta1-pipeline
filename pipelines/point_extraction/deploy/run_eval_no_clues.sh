c518adb4d558cf67c49aff4d3575664295aa4f9d
Chris Bethune <cbethune@uncharted.software>
2024-07-16 12:20:33 -0400

    adds no hint run script for points

---
+++ pipelines/point_extraction/deploy/run_eval_no_clues.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# args: $1 - path to local directory to mount as /input in docker container
+#       $2 - path to local directory to mount as /output in docker container
+#       $3 - path to local directory to mount as /workdir in docker container
+
+# ensure that an the GOOGLE_APPLICATION_CREDENTIALS env var is set
+if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+    echo "GOOGLE_APPLICATION_CREDENTIALS env var must be set to the path of the GCP service account key"
+    exit 1
+fi
+
+# ensure that the args are present
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Usage: $0 <input_dir> <output_dir> <workdir_dir>"
+    exit 1
+fi
+
+docker network ls | grep -q 'lara' || docker network create lara
+docker run \
+    --pull always \
+    --rm \
+    --name point_extraction \
+    --entrypoint python3 \
+    -e GOOGLE_APPLICATION_CREDENTIALS=/credentials.json \
+    -v $GOOGLE_APPLICATION_CREDENTIALS:/credentials.json \
+    -v $1:/input \
+    -v $2:/output \
+    -v $3:/workdir \
+    --net lara \
+    uncharted/lara-point-extract:eval \
+         -m pipelines.point_extraction.run_pipeline \
+        --input /input \
+        --output /output \
+        --workdir /workdir \
+        --bitmasks \
+        --model_point_extractor pipelines/point_extraction_weights/lara_yolo9_20240702_best.pt \
+        --model_segmenter pipelines/segmentation_weights/layoutlmv3_20240531
