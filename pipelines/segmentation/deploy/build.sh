60648a880e78c64f00cf8e291c149448a7ee623b
Chris Bethune <cbethune@uncharted.software>
2024-08-20 09:12:47 -0400

    undo buildx changes

--- pipelines/segmentation/deploy/build.sh
+++ pipelines/segmentation/deploy/build.sh
@@ -22,7 +22,7 @@ else
 fi
 
 # run the build
-docker buildx build --platform linux/amd64,linux/arm64 -t uncharted/lara-segmentation:latest . --push
+docker build -t uncharted/lara-segmentation:latest .
 
 # cleanup the temp files
 rm -rf pipelines
