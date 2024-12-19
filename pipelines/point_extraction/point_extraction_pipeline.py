5cb47aab21635d1ae8f75490250c6e0982dc773e
David Giesbrecht <dgiesbecht@uncharted.software>
2024-12-19 09:53:54 -0500

    Added a --debug_images option to the points pipeline  (similar to the segmentation pipeline). This option outputs viz images showing point extraction bboxes overlayed on each map. Off by default

--- pipelines/point_extraction/point_extraction_pipeline.py
+++ pipelines/point_extraction/point_extraction_pipeline.py
@@ -1,9 +1,11 @@
 import logging
+import math
+from collections import defaultdict
 from typing import List
-
+from PIL import ImageDraw, ImageFont
 from schema.mappers.cdr import PointsMapper
 from tasks.common.io import append_to_cache_location
-from tasks.common.task import EvaluateHalt, HaltPipeline
+from tasks.common.task import EvaluateHalt
 from tasks.segmentation.segmenter_utils import map_missing
 from tasks.point_extraction.legend_analyzer import (
     LegendPreprocessor,
@@ -11,7 +13,10 @@ from tasks.point_extraction.legend_analyzer import (
 )
 from tasks.point_extraction.point_extractor import YOLOPointDetector
 from tasks.point_extraction.point_orientation_extractor import PointOrientationExtractor
-from tasks.point_extraction.point_extractor_utils import convert_preds_to_bitmasks
+from tasks.point_extraction.point_extractor_utils import (
+    convert_preds_to_bitmasks,
+    rotated_about,
+)
 from tasks.point_extraction.template_match_point_extractor import (
     TemplateMatchPointExtractor,
 )
@@ -24,12 +29,14 @@ from tasks.point_extraction.entities import (
     MAP_PT_LABELS_OUTPUT_KEY,
 )
 from tasks.common.pipeline import (
+    EmptyOutput,
     BaseModelOutput,
     Pipeline,
     PipelineResult,
     OutputCreator,
     Output,
     ImageDictOutput,
+    ImageOutput,
 )
 from tasks.segmentation.detectron_segmenter import DetectronSegmenter
 from tasks.segmentation.denoise_segments import DenoiseSegments
@@ -67,6 +74,7 @@ class PointExtractionPipeline(Pipeline):
         gpu=True,
         batch_size=20,
         metrics_url="",
+        debug_images=False,
     ):
         # extract text from image, segmentation to only keep the map area,
         # tile, extract points, untile, predict direction
@@ -131,6 +139,8 @@ class PointExtractionPipeline(Pipeline):
             outputs.append(CDROutput("map_point_label_cdr_output"))
         if include_bitmasks_output:
             outputs.append(BitmasksOutput("map_point_label_bitmasks_output"))
+        if debug_images:
+            outputs.append(DebugImagesOutput("map_point_label_debug_images_output"))
 
         super().__init__("point_extraction", "Point Extraction", outputs, tasks)
         self._verbose = verbose
@@ -238,4 +248,132 @@ class BitmasksOutput(OutputCreator):
 
         return ImageDictOutput(
             pipeline_result.pipeline_id, pipeline_result.pipeline_name, bitmasks_dict
+        )
+
+
+class DebugImagesOutput(OutputCreator):
+    def __init__(self, id: str, draw_rotated_rectangles: bool = True):
+        self._draw_rotated_rectangles = draw_rotated_rectangles
+        super().__init__(id)
+
+    def create_output(self, pipeline_result: PipelineResult) -> Output:
+        """
+        Creates an output image with point extraction bboxes overlayed
+
+        Args:
+            pipeline_result (PipelineResult): The pipeline result.
+
+        Returns:
+            ImageOutput: An image showing with point extraction bboxes overlayed on the map
+        """
+        logger.info(
+            f"Creating point extraction viz image for raster: {pipeline_result.raster_id}..."
+        )
+
+        class2colour = {
+            "strike_and_dip": "red",
+            "horizontal_bedding": "blue",
+            "overturned_bedding": "green",
+            "vertical_bedding": "orange",
+            "inclined_foliation": "darkmagenta",
+            "inclined_foliation_igneous": "limegreen",
+            "vertical_foliation": "springgreen",
+            "vertical_joint": "turquoise",
+            "gravel_borrow_pit": "hotpink",
+            "mine_shaft": "darkviolet",
+            "prospect": "deepskyblue",
+            "mine_tunnel": "tomato",
+            "mine_quarry": "limegreen",
+            "sink_hole": "goldenrod",
+            "lineation": "violet",
+            "drill_hole": "cyan",
+        }
+        font_size = 16
+        line_width = 4
+        font = ImageFont.load_default(font_size)
+
+        result = pipeline_result.data.get(MAP_PT_LABELS_OUTPUT_KEY, None)
+        if result is None:
+            return EmptyOutput()
+        map_point_labels = PointLabels.model_validate(result)
+
+        if pipeline_result.image is None:
+            raise ValueError("Pipeline result image is None")
+        debug_image = pipeline_result.image.copy()
+        draw = ImageDraw.Draw(debug_image, mode="RGBA")
+
+        if not map_point_labels.labels:
+            return EmptyOutput()
+
+        counts_per_class = defaultdict(int)
+        for label in map_point_labels.labels:
+
+            box_coords = (label.x1, label.y1, label.x2, label.y2)
+            class_name = label.class_name
+            # score = label.score
+            if class_name not in class2colour:
+                logger.warning(
+                    f"Point class_name not recognized: {class_name}. Plotting with yellow"
+                )
+                colour = "yellow"
+            else:
+                colour = class2colour[class_name]
+            if self._draw_rotated_rectangles and label.direction:
+                rot_angle_compass = float(label.direction)
+                # convert angle compass angle convention (CW with 0 deg at top)
+                # regular 'trig' angle convention (CCW with 0 to the right)
+                rot_angle = 270 - rot_angle_compass
+                if rot_angle < 0:
+                    rot_angle += 360
+                xc = (label.x1 + label.x2) / 2.0
+                yc = (label.y1 + label.y2) / 2.0
+                square_vertices = [
+                    (label.x1, label.y1),
+                    (label.x2, label.y1),
+                    (label.x2, label.y2),
+                    (label.x1, label.y2),
+                ]
+                square_vertices = [
+                    rotated_about(x, y, xc, yc, math.radians(-rot_angle))
+                    for x, y in square_vertices
+                ]
+                draw.polygon(square_vertices, outline=colour, width=line_width)
+            else:
+                draw.rectangle(box_coords, outline=colour, width=line_width)
+
+            class_text = class_name.replace("_", " ")
+            draw.text(
+                (label.x1, label.y1 - font_size),
+                class_text,
+                fill=colour,
+                font=font,
+                font_size=font_size,
+            )
+
+            angles_text = ""
+            # don't write strike angles for now (shown by rotated rect)
+            # if label["direction"]:
+            #    angles_text = f'angle: {int(label["direction"])}'
+            if label.dip:
+                if angles_text:
+                    angles_text += ", "
+                angles_text += f"dip: {int(label.dip)}"
+            if angles_text:
+                draw.text(
+                    (label.x1, label.y2),
+                    angles_text,
+                    fill=colour,
+                    font=font,
+                    font_size=font_size,
+                )
+
+            counts_per_class[class_name] += 1
+
+        print("--------")
+        print(f"Point Extractions per class for raster {pipeline_result.raster_id}:")
+        for label, num in counts_per_class.items():
+            print(f"{label}:  {num}")
+
+        return ImageOutput(
+            pipeline_result.pipeline_id, pipeline_result.pipeline_name, debug_image
         )
