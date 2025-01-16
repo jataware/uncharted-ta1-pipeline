8830a7e2c7bef56a6bef50a16134fd73c74b000e
David Giesbrecht <dgiesbecht@uncharted.software>
2025-01-16 11:55:23 -0500

    added scripts and readme for 18-month self-eval for points extraction

--- eval/18month_eval/eval_utils.py
+++ eval/18month_eval/eval_utils.py
@@ -1,6 +1,8 @@
 import math
 from geopy.distance import geodesic
 from rasterio.transform import from_gcps, AffineTransformer
+from typing import List
+import re
 
 
 def score_query_points(gcps) -> float:
@@ -37,3 +39,50 @@ def score_query_points(gcps) -> float:
 
     rmse = math.sqrt(sum_sq_error / num_pts)
     return rmse
+
+
+def point_property_for_grouping(props: List[str]) -> str:
+    """
+    choose an appropriate property for grouping point features from a shapefile
+    (ie if a single shapefile contains different point symbol types)
+    """
+    # bad_list = ["dip", "strike", "angle", "id"]
+    # ranked list of 'good' point type property labels
+    good_list = [
+        "type",
+        "ftr_type",
+        "pointtype",
+        "pttype",
+        "ptype",
+        "stype",
+        "feature_type",
+        "descriptio",
+        "description",
+        "feature",
+    ]
+
+    if not props:
+        print("No shapefile properties to group by")
+        return ""
+
+    print(props)
+    for g in good_list:
+        for p in props:
+            p_norm = p.lower().strip()
+            if p_norm == g:
+                print(f"Grouping point features by property: {p}")
+                return p
+
+    return ""
+
+
+# from https://stackoverflow.com/questions/1007481/how-to-replace-whitespaces-with-underscore
+def urlify(s):
+
+    # Remove all non-word characters (everything except numbers and letters)
+    s = re.sub(r"[^\w\s]", "", s)
+
+    # Replace all runs of whitespace with a single dash
+    s = re.sub(r"\s+", "-", s)
+
+    return s
